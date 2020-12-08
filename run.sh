c8750b787f1fdfda8c95603192975479b428a575
garrett4wade <fuwth17@gmai.com>
2020-12-08 18:22:32 +0800

    subproc env is slow

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
-# num_workers=32
+pkill -9 ray
+num_workers=32
 cpu_per_worker=1
-num_envs=(4 16)
-group_name="memory"
-job_name="circularbuffer_list"
+num_envs=(2 4 8 16)
+group_name="subproc"
+job_name="subproc"
 num_frames=4000000
 seed=562789
 for num_env in ${num_envs[@]}
 do
-    num_workers=$((32*8/${num_env}))
-    exp_name="10M_circularbuffer_list_256worker"${num_workers}
+    exp_name="env"${num_env}"worker"${num_workers}
     echo "current experiment ${exp_name}"
     python run_async_ppo.py --exp_name ${exp_name} \
                             --wandb_group ${group_name} \
@@ -19,6 +19,7 @@ do
                             --num_workers ${num_workers} \
                             --cpu_per_worker ${cpu_per_worker} \
                             --q_size 16 \
-                            --gpu_id 0
+                            --gpu_id 0 \
+                            --use_subproc
     pkill -9 ray
 done
