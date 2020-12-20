8f20a760ad2957c785ed9c0446b746c88f6187f2
garrett4wade <fuwth17@gmai.com>
2020-12-20 20:23:00 +0800

    add mujoco

---
+++ env/mujoco/model/model.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.distributions import Normal
+
+# borrowed from soft-actor-critic
+LOG_STD_MAX = 2
+LOG_STD_MIN = -20
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, is_training, kwargs):
+        super().__init__()
+        self.is_training = is_training
+        if not is_training:
+            self.device = torch.device('cpu')
+        else:
+            self.device = torch.device(kwargs['gpu_id'] if torch.cuda.is_available() else 'cpu')
+
+        action_scale = torch.from_numpy(kwargs['action_scale'].copy())
+        action_loc = torch.from_numpy(kwargs['action_loc'].copy())
+        self.action_scale = nn.Parameter(action_scale).detach().to(self.device)
+        self.action_loc = nn.Parameter(action_loc).detach().to(self.device)
+
+        state_dim = kwargs['state_dim']
+        action_dim = kwargs['action_dim']
+        hidden_dim = kwargs['hidden_dim']
+
+        self.action_dim = kwargs['action_dim']
+        self.feature_net = nn.Sequential(nn.Linear(state_dim, hidden_dim), nn.LayerNorm(normalized_shape=[hidden_dim]),
+                                         nn.ReLU(), nn.Linear(hidden_dim, hidden_dim),
+                                         nn.LayerNorm(normalized_shape=[hidden_dim]), nn.ReLU())
+        # actor
+        self.action_layer = nn.Linear(hidden_dim, 2 * action_dim)
+        # critic
+        self.value_layer = nn.Linear(hidden_dim, 1)
+
+        self.clip_ratio = kwargs['clip_ratio']
+        self.tpdv = dict(device=self.device, dtype=torch.float32)
+        self.to(self.device)
+
+    def core(self, state):
+        return self.feature_net(state)
+
+    def forward(self, state):
+        x = self.core(state)
+        action_logits = self.action_layer(x)
+        value = self.value_layer(x).squeeze(-1)
+
+        mu, log_std = torch.split(action_logits, self.action_dim, dim=-1)
+        log_std = log_std.clamp(LOG_STD_MIN, LOG_STD_MAX)
+        mu = torch.tanh(mu) * self.action_scale + self.action_loc
+        dist = Normal(loc=mu, scale=log_std.exp())
+
+        action = dist.sample()
+
+        return action, action_logits, value
+
+    @torch.no_grad()
+    def value(self, obs):
+        assert not self.is_training
+        obs = torch.from_numpy(obs)
+        return self.value_layer(self.core(obs)).item()
+
+    @torch.no_grad()
+    def select_action(self, obs):
+        assert not self.is_training
+        obs = torch.from_numpy(obs)
+        results = self(obs)
+        return [result.numpy() for result in results]
+
+    def compute_loss(self, obs, action, action_logits, adv, value, value_target):
+        adv = (adv - adv.mean()) / (adv.std() + 1e-8)
+        # to remove padding values
+        valid_mask = (value != 0.0).to(torch.float32)
+        _, target_action_logits, cur_state_value = self(obs)
+
+        target_mu, target_log_std = torch.split(target_action_logits, self.action_dim, dim=-1)
+        target_mu = torch.tanh(target_mu) * self.action_scale + self.action_loc
+        target_log_std = target_log_std.clamp(LOG_STD_MIN, LOG_STD_MAX)
+        target_dist = Normal(target_mu, target_log_std.exp())
+        target_action_logprobs = target_dist.log_prob(action).sum(-1)
+
+        mu, log_std = torch.split(action_logits, self.action_dim, dim=-1)
+        mu = torch.tanh(mu) * self.action_scale + self.action_loc
+        log_std = log_std.clamp(LOG_STD_MIN, LOG_STD_MAX)
+        behavior_dist = Normal(mu, log_std.exp())
+        behavior_action_logprobs = behavior_dist.log_prob(action).sum(-1)
+
+        log_rhos = target_action_logprobs - behavior_action_logprobs.detach()
+        rhos = log_rhos.exp()
+
+        p_surr = adv * torch.clamp(rhos, 1 - self.clip_ratio, 1 + self.clip_ratio)
+        p_loss = (-torch.min(p_surr, rhos * adv) * valid_mask).mean()
+
+        cur_v_clipped = value + torch.clamp(cur_state_value - value, -self.clip_ratio, self.clip_ratio)
+        v_loss = torch.max(F.mse_loss(cur_state_value, value_target), F.mse_loss(cur_v_clipped, value_target))
+        v_loss = (v_loss * valid_mask).mean()
+
+        entropy_loss = (-target_dist.entropy().sum(-1) * valid_mask).mean()
+        return v_loss, p_loss, entropy_loss
+
+    def get_weights(self):
+        return {k: v.cpu() for k, v in self.state_dict().items()}
---
+++ run_mujoco.sh
@@ -0,0 +1,27 @@
+pkill -9 ray
+num_workers=(48)
+num_env=16
+min_return_chunk_num=32
+group_name="scalability"
+job_name="scalability"
+num_frames=10000000
+seed=986258
+for num_worker in ${num_workers[@]}
+do
+    exp_name="release_128humanoid_env"${num_env}"worker"${num_worker}
+    echo "current experiment ${exp_name}"
+    python run_async_ppo.py --exp_name ${exp_name} \
+                            --wandb_group ${group_name} \
+                            --wandb_job ${job_name} \
+                            --total_frames ${num_frames} \
+                            --seed ${seed} \
+                            --env_num ${num_env} \
+                            --num_workers ${num_worker} \
+                            --gpu_id 0 \
+                            --min_return_chunk_num ${min_return_chunk_num} \
+                            --batch_size 128 \
+                            --num_collectors 8 \
+                            --num_readers 4 \
+                            --no_summary
+    pkill -9 ray
+done
