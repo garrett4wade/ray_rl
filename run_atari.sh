39c3b153e1025fb4d2bc2146e3f330a2dba369ab
garrett4wade <fuwth17@gmai.com>
2021-01-10 21:37:52 +0800

    release

--- run_atari.sh
+++ run_atari.sh
@@ -9,7 +9,7 @@ num_frames=100000000
 seed=678446
 for num_worker in ${num_workers[@]}
 do
-    exp_name="2Sup_breakout_"${num_env}"*"${num_worker}
+    exp_name="4SupWrtier_breakout_"${num_env}"*"${num_worker}
     echo "current experiment ${exp_name}"
     python3.8 main_atari.py --exp_name ${exp_name} \
                             --wandb_group ${group_name} \
