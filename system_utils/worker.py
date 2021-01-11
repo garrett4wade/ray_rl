54d2d22e4b8219de4c4a62da06b99bd36ba94e59
garrett4wade <fuwth17@gmai.com>
2021-01-11 20:50:00 +0800

    remote rollout worker definition via decorator

--- system_utils/worker.py
+++ system_utils/worker.py
@@ -1,6 +1,7 @@
 import ray
 
 
+@ray.remote(num_cpus=1)
 class RolloutWorker:
     def __init__(self, worker_id, model_fn, worker_env_fn, ps, kwargs):
         self.id = worker_id
@@ -50,11 +51,11 @@ class RolloutCollector:
     def __init__(self, supervisor_id, model_fn, worker_env_fn, ps, kwargs):
         self.num_workers = kwargs['num_workers'] // kwargs['num_supervisors']
         self.workers = [
-            ray.remote(num_cpus=1)(RolloutWorker).remote(worker_id=i + supervisor_id * kwargs['num_supervisors'],
-                                                         model_fn=model_fn,
-                                                         worker_env_fn=worker_env_fn,
-                                                         ps=ps,
-                                                         kwargs=kwargs) for i in range(self.num_workers)
+            RolloutWorker.remote(worker_id=i + supervisor_id * kwargs['num_supervisors'],
+                                 model_fn=model_fn,
+                                 worker_env_fn=worker_env_fn,
+                                 ps=ps,
+                                 kwargs=kwargs) for i in range(self.num_workers)
         ]
         self.working_jobs = []
         # job_hashing maps job id to worker index
