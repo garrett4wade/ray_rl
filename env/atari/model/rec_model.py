monte carlo vtarget; debug gae computation; add multigpu adv normalization; fix ps to head

env/atari/model/rec_model.py
@@ -1,8 +1,10 @@
 import torch
+import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.distributions import Categorical
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 
 class ActorCritic(nn.Module):
@@ -58,10 +60,26 @@ class ActorCritic(nn.Module):
         return action.numpy(), action_logits.numpy(), value.numpy(), h_out.numpy()
 
 
-def compute_loss(learner, obs, action, action_logits, adv, value, value_target, pad_mask, rnn_hidden, clip_ratio):
+def compute_loss(learner, obs, action, action_logits, adv, value, value_target, pad_mask, rnn_hidden, clip_ratio,
+                 world_size):
+    if isinstance(learner, DDP):
+        assert world_size > 1
+        adv_mean = adv.mean()
+        adv_meansq = adv.pow(2).mean()
+
+        # all reduce to advantage mean & std
+        dist.all_reduce_multigpu([adv_mean])
+        dist.all_reduce_multigpu([adv_meansq])
+        adv_mean = adv_mean / world_size
+        adv_meansq = adv_meansq / world_size
+
+        adv_std = (adv_meansq - adv_mean.pow(2)).sqrt()
+    else:
+        adv_mean = adv.mean()
+        adv_std = adv.std(unbiased=False)
+    adv = (adv - adv_mean) / (adv_std + 1e-8)
     obs = obs.to(torch.float32) / 255
     action = action.to(torch.long)
-    adv = (adv - adv.mean()) / (adv.std() + 1e-8)
 
     target_action_logits, cur_state_value, _ = learner(obs, rnn_hidden)
 

env/mujoco/model/model.py
@@ -1,4 +1,5 @@
 import torch
+import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Normal
@@ -56,14 +57,27 @@ class ActorCritic(nn.Module):
         return [result.numpy() for result in results]
 
 
-def compute_loss(learner, obs, action, action_logits, adv, value, value_target, pad_mask, clip_ratio):
+def compute_loss(learner, obs, action, action_logits, adv, value, value_target, pad_mask, clip_ratio, world_size):
     if isinstance(learner, DDP):
+        assert world_size > 1
         action_scale = learner.module.action_scale
         action_loc = learner.module.action_loc
+        adv_mean = adv.mean()
+        adv_meansq = adv.pow(2).mean()
+
+        # all reduce to advantage mean & std
+        dist.all_reduce_multigpu([adv_mean])
+        dist.all_reduce_multigpu([adv_meansq])
+        adv_mean = adv_mean / world_size
+        adv_meansq = adv_meansq / world_size
+
+        adv_std = (adv_meansq - adv_mean.pow(2)).sqrt()
     else:
         action_scale = learner.action_scale
         action_loc = learner.action_loc
-    adv = (adv - adv.mean()) / (adv.std() + 1e-8)
+        adv_mean = adv.mean()
+        adv_std = adv.std(unbiased=False)
+    adv = (adv - adv_mean) / (adv_std + 1e-8)
     _, target_action_logits, cur_state_value = learner(obs)
 
     action_dim = action_logits.shape[-1] // 2

trainer/trainer.py
@@ -91,7 +91,10 @@ class Trainer:
 
     def train_learner_on_batch(self):
         self.optimizer.zero_grad()
-        v_loss, p_loss, entropy_loss = self.loss_fn(self.learner, clip_ratio=self.config.clip_ratio, **self.tensor_dict)
+        v_loss, p_loss, entropy_loss = self.loss_fn(self.learner,
+                                                    clip_ratio=self.config.clip_ratio,
+                                                    world_size=self.world_size,
+                                                    **self.tensor_dict)
         loss = p_loss + self.config.value_coef * v_loss + self.config.entropy_coef * entropy_loss
         loss.backward()
         grad_norm = torch.nn.utils.clip_grad_norm_(self.learner.parameters(), self.config.max_grad_norm)
@@ -129,10 +132,10 @@ class Trainer:
                 dist.all_reduce_multigpu([entropy_loss])
                 dist.all_reduce_multigpu([grad_norm])
             loss_stat = {
-                'v_loss': v_loss.item(),
-                'p_loss': p_loss.item(),
-                'entropy_loss': entropy_loss.item(),
-                'grad_norm': grad_norm.item()
+                'v_loss': v_loss.item() / self.world_size,
+                'p_loss': p_loss.item() / self.world_size,
+                'entropy_loss': entropy_loss.item() / self.world_size,
+                'grad_norm': grad_norm.item() / self.world_size
             }
             iter_dur = time.time() - iter_start
 