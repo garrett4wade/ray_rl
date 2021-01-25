28721e7cd011f3eed973b35f46408ac430f35ae4
garrett4wade <fuwth17@gmai.com>
2021-01-25 21:08:43 +0800

    atari release

--- main_atari.py
+++ main_atari.py
@@ -10,7 +10,7 @@ from env.atari.registry import get_shapes, ROLLOUT_KEYS, COLLECT_KEYS, DTYPES, I
 from env.atari.model.rec_model import ActorCritic, compute_loss
 from env.atari.wrappers import WarpFrame, FrameStack
 from system_utils.rollout_runner import RolloutRunner
-from trainer import Trainer
+from trainer.trainer import Trainer
 from utils.find_free_gpu import find_free_gpu
 
 # global configuration
@@ -53,6 +53,7 @@ parser.add_argument('--ray_dashboard', action='store_true', help='use ray dashbo
 parser.add_argument('--num_writers', type=int, default=4, help='# of buffer writers')
 parser.add_argument('--push_period', type=int, default=1, help='learner parameter upload period')
 parser.add_argument('--num_workers', type=int, default=32, help='remote worker numbers')
+parser.add_argument('--num_postprocessors', type=int, default=2, help='remote worker numbers')
 parser.add_argument('--cpu_per_worker', type=int, default=1, help='cpu used per worker')
 parser.add_argument('--q_size', type=int, default=8, help='number of batches in replay buffer')
 
@@ -140,7 +141,8 @@ if __name__ == "__main__":
                 weights_available=rollout_runner.weights_available,
                 ep_info_dict=rollout_runner.ep_info_dict,
                 queue_util=rollout_runner.queue_util,
-                wait_time=rollout_runner.wait_time,
+                worker_wait_time=rollout_runner.worker_wait_time,
+                postprocessor_wait_time=rollout_runner.postprocessor_wait_time,
                 config=config) for rank in ranks
     ]
     jobs = [mp.Process(target=trainer.run) for trainer in trainers]
--- run_atari.sh
+++ run_atari.sh
@@ -1,15 +1,15 @@
 pkill -9 ray
 pkill -9 python3.8
 rm -rf /dev/shm/*
-num_workers=(32)
+num_workers=(48)
 num_env=16
-group_name="shm_buf"
-job_name="shm_buf"
+group_name="postprocessor"
+job_name="postprocessor"
 num_frames=100000000
 seed=678446
 for num_worker in ${num_workers[@]}
 do
-    exp_name="2writer_nolazyreturn_breakout_"${num_env}"*"${num_worker}
+    exp_name="post2wrt2_breakout_"${num_env}"*"${num_worker}
     echo "current experiment ${exp_name}"
     python3.8 main_atari.py --exp_name ${exp_name} \
                             --wandb_group ${group_name} \
@@ -22,6 +22,8 @@ do
                             --batch_size 128 \
                             --q_size 16 \
                             --num_writers 2 \
+                            --num_gpus 1 \
+                            --num_postprocessors 2 \
                             --no_summary
     pkill -9 ray
     pkill -9 python3.8
